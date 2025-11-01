56db137b6200dd9acce6c4b31c426f21223f88e9
Kshitij Yelpale <kshitij.yelpale@gmail.com>
2025-11-01 10:56:12 +0100

    Initial commit

---
+++ src/main/java/dev/yelpalekshitij/githubreposcorer/GithubRepoScorerApplication.java
@@ -0,0 +1,13 @@
+package dev.yelpalekshitij.githubreposcorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepoScorerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubRepoScorerApplication.class, args);
+    }
+
+}
